e4182f116b543fb1e7f819e5e0fc0f8d1ed69718
ZainAI786 <abidasaleem246@gmail.com>
2025-02-14 21:17:22 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://cdn.ironman.my.id/i/ipmxt9.jpg"
+    ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am Alive. Bot created by ⸙ 𝐌𝐑־ 𓆩«𝚭𝚫𝚰𝚴𔒝»ꜛ؜𓆪"
 };
